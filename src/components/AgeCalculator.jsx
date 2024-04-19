6706b62e5b0e5663e89cfa763ce53cd9dedd2e84
Ryan Alfred Taganas <84939129+ryanalfredtaganas@users.noreply.github.com>
2024-04-19 13:06:52 +0800

    Update AgeCalculator.jsx

--- src/components/AgeCalculator.jsx
+++ src/components/AgeCalculator.jsx
@@ -104,7 +104,7 @@ const AgeCalculator = () => {
             <img
               onClick={calculateAge}
               className="arrow"
-              src="/assets/icon-arrow.svg"
+              src="../src/assets/icon-arrow.svg"
               alt=""
               srcset=""
             />
